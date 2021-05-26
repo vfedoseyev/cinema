51a365abb262090250838546edf94e96c9114970
mrfed <mr.fedoseyev@mail.ru>
2021-05-26 14:42:11 +0300

    chage owl carousel

--- films-table.js
+++ films-table.js
@@ -65,84 +65,47 @@ const films = [{
         },
     ]
 
-}, ];
-const tableBody = document.getElementById('block03-table-body')
-tableBody.innerHTML = `
+}, {
+    start: '01:00',
+    title: 'XXX',
+    adult: true,
+    genre: [{
+            name: 'фантастика'
+
+        },
+        {
+            name: 'боевик'
+
+        },
+        {
+            name: 'приключения'
+
+        },
+    ]
+}];
+
+function renderFilmTableItem(film) {
+    return `
     <tr>
-                         <td class="fixed-size">
-                            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                    fill="white" />
-                   </svg>
+    <td class="fixed-size">
+        <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
+                fill="white" />
+        </svg>
 
-                        </td>
-                        <td>${films[0].start}</td>
-                        <td>${films[0].title}</td>
-                        <td>${films[0].genre.map(g=>g.name)}</td>
-                    </tr>
-`;
-tableBody.innerHTML += `
-    <tr>
-                         <td class="fixed-size">
-                            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                    fill="white" />
-                   </svg>
+    </td>
+    <td>${film.start}</td>
+    <td>${film.title}</td>
+    <td>${film.genre.map(g => g.name)}</td>
+    </tr>
+`
+}
+const tableBody = document.getElementById('block03-table-body');
+tableBody.innerHTML = '';
 
-                        </td>
-                        <td>${films[1].start}</td>
-                        <td>${films[1].title}</td>
-                        <td>${films[1].genre.map(g=>g.name)}</td>
-                    </tr>   
-`;
-tableBody.innerHTML += `
-    <tr>
-                         <td class="fixed-size">
-                            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                    fill="white" />
-                   </svg>
-
-                        </td>
-                        <td>${films[2].start}</td>
-                        <td>${films[2].title}</td>
-                        <td>${films[2].genre.map(g=>g.name)}</td>
-                    </tr>   
-`;
-tableBody.innerHTML += `
-    <tr>
-                         <td class="fixed-size">
-                            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                    fill="white" />
-                   </svg>
-
-                        </td>
-                        <td>${films[3].start}</td>
-                        <td>${films[3].title}</td>
-                        <td>${films[3].genre.map(g=>g.name)}</td>
-                    </tr>   
-`;
-
-for (let index = 0; index < films; index++) {
-    `
-    <tr>
-                         <td class="fixed-size">
-                            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                    fill="white" />
-                   </svg>
-
-                        </td>
-                        <td>${films[index].start}</td>
-                        <td>${films[index].title}</td>
-                        <td>${films[index].genre.map(g=>g.name)}</td>
-                    </tr>   
-`;
-
+for (let index = 0; index < films.length; index++) {
+    if (!films[index].adult) {
+        tableBody.innerHTML += renderFilmTableItem(films[index]);
+    }
 }
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(".owl-carousel").owlCarousel({
     loop: true,
     nav: true,
-    dots: false
+    dots: false,
+    center: true,
+    responsive: {
+        0: {
+          items: 1
+        },
+        769: {
+          items: 2
+        },
+        1124: {
+          items: 3
+        }
+      }
 });
