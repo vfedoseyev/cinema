8715a2cb9db4082d1810c28381641e92af7fe204
mrfed <mr.fedoseyev@mail.ru>
2021-06-03 19:42:12 +0300

    change table in js file

--- films-table.js
+++ films-table.js
@@ -84,25 +84,46 @@ const filmList = [{
     ]
 }];
 
+const filmHelper = {
+    getId() {
+        return this.id || this.title.replaceAll(' ', '-');
+    },
+    getTitle() {
+        return this.title;
+    },
+    getStart() {
+        return this.start;
+    },
+    getGenre() {
+        return this.genre
+            .map(g => g.name)
+            .join(', ');
+    },
+}
+
+function renderFilmTableItem(film) {
+    return `
+    <tr>
+        <td class="fixed-size">
+        <input type="checkbox" id="${filmHelper.getId.apply(film)}">
+        <label for="${filmHelper.getId.apply(film)}">
+            <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
+                    fill="white" />
+            </svg>
+        </label>
+        </td>
+        <td>${filmHelper.getStart.apply(film)}</td>
+        <td><a href="https://www.kinopoisk.ru/">${filmHelper.getTitle.apply(film)}</a></td>
+        <td>${filmHelper.getGenre.apply(film)}</td>
+    </tr>`
+}
+
 const tableBody = document.getElementById('block03-table-body');
 tableBody.innerHTML = '';
 
-
 for (const film of filmList) {
-    tableBody.innerHTML += `
-<tr>
-    <td class="fixed-size">
-    <input type="checkbox" id="block03__checkbox1">
-    <label for="block03__checkbox1">
-        <svg viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                fill="white" />
-        </svg>
-    </label>
-    </td>
-    <td>${film.start}</td>
-    <td><a href="https://www.kinopoisk.ru/">${film.title}</a></td>
-    <td>${film.genre.map(g=>g.name)}</td>
-</tr>`;
+    if (!film.adult)
+        tableBody.innerHTML += renderFilmTableItem(film);
 }
