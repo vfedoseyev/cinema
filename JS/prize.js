0d4313a8160bdf4012bccd57b66541d93ed69807
mr.fedoseyev <mr.fedoseyev@mail.ru>
2021-06-06 18:32:08 +0300

    add some changes

--- JS/prize.js
+++ JS/prize.js
@@ -1,9 +1,50 @@
 const openBtn = document.getElementById("btn-prize-open");
 const popup = document.querySelector("#prize-popup");
 const closeBtn = document.querySelector("#prize-popup__close");
+const nameField = document.querySelector('#prize-popup input[name="name"]').parentNode;
+const emailField = document.querySelector('#prize-popup input[name="email"]').parentNode;
+const form = document.getElementById('#prize-form');
 
 function popupToggle() {
-    popup.classList.toggle('hidden')
+    popup.classList.toggle('hidden');
 }
+const ERROR_CLASS_NAME = 'st-input1_error';
+const FOCUSED_CLASS_NAME = 'st-input1_focused';
+
+function initializeField(field) {
+    const input = field.getElementsByTagName('input')[0];
+    const fieldError = field.querySelector('st-input1__error-msg');
+    input.value = '';
+    field.classList.remove(ERROR_CLASS_NAME);
+    field.classList.remove(FOCUSED_CLASS_NAME);
+    fieldError.innerText = '';
+    input.onfocus = function () {
+        field.classList.add(FOCUSED_CLASS_NAME)
+    }
+    input.onBlur = () => {
+        if (!input.value) {
+            field.classList.remove(FOCUSED_CLASS_NAME);
+        }
+    }
+    return {
+        getValue() {
+            return input.value
+        }
+    }
+}
+const nameFieldUtils = initializeField(nameField);
+const emailFieldUtils = initializeField(nameField);
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+        name: nameFieldUtils.getValue(),
+        email: emailFieldUtils.getValue(),
+    }
+    const url = new URL('http://inno-ijl.ru/multystub/stc-21-03/feedback');
+    url.search = new URLSearchParams(data).toString();
+    fetch(url.toString());
+}
+form.addEventListener('submit', handleSubmit);
--- target/prize.js
+++ target/prize.js
@@ -3,10 +3,56 @@
 var openBtn = document.getElementById("btn-prize-open");
 var popup = document.querySelector("#prize-popup");
 var closeBtn = document.querySelector("#prize-popup__close");
+var nameField = document.querySelector('#prize-popup input[name="name"]').parentNode;
+var emailField = document.querySelector('#prize-popup input[name="email"]').parentNode;
+var form = document.getElementById('#prize-form');
 
 function popupToggle() {
   popup.classList.toggle('hidden');
 }
 
+var ERROR_CLASS_NAME = 'st-input1_error';
+var FOCUSED_CLASS_NAME = 'st-input1_focused';
+
+function initializeField(field) {
+  var input = field.getElementsByTagName('input')[0];
+  var fieldError = field.querySelector('st-input1__error-msg');
+  input.value = '';
+  field.classList.remove(ERROR_CLASS_NAME);
+  field.classList.remove(FOCUSED_CLASS_NAME);
+  fieldError.innerText = '';
+
+  input.onfocus = function () {
+    field.classList.add(FOCUSED_CLASS_NAME);
+  };
+
+  input.onBlur = function () {
+    if (!input.value) {
+      field.classList.remove(FOCUSED_CLASS_NAME);
+    }
+  };
+
+  return {
+    getValue: function getValue() {
+      return input.value;
+    }
+  };
+}
+
+var nameFieldUtils = initializeField(nameField);
+var emailFieldUtils = initializeField(nameField);
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var data = {
+    name: nameFieldUtils.getValue(),
+    email: emailFieldUtils.getValue()
+  };
+  var url = new URL('http://inno-ijl.ru/multystub/stc-21-03/feedback');
+  url.search = new URLSearchParams(data).toString();
+  fetch(url.toString());
+}
+
+form.addEventListener('submit', handleSubmit);
